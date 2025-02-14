61e755fd79b34b39ff23a7bdade26a75d9ea0f00
abhik345 <abhikdas345@gmail.com>
2025-02-14 18:04:29 +0530

    comment change route

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -5,8 +5,6 @@ import {createComment,getComments} from "../controllers/comment.controller"
 const router = Router();
 
 router.post("/create", createComment);
-// router.get("/get-all-user/:userId", getCommentsByUser);
-// router.get("/get-all-post/:postId", getCommentsByPost);
 router.get("/get-all", getComments);
 
 
